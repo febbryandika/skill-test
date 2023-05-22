a0daae178ebd2d896e6e3e320f96a58072add434
Febbry Andika <febbryandika97@gmail.com>
2023-05-22 13:54:15 +0700

    fix(components): rename Header to PriceListHeader

---
+++ src/components/PriceListHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PriceListHeader() {
+  return (
+    <div className="price-list-header">
+      <h1>Data Harga Ikan</h1>
+    </div>
+  );
+}
+
+export default PriceListHeader;
